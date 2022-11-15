6d6a7835ba476223bf906dce43781b9fef0d32b5
QSD_Tina <tina@qsdbih.com>
2022-11-15 13:00:16 +0100

    Testing commit and push

--- app/src/main/java/com/example/qsdmovies/MainActivity.kt
+++ app/src/main/java/com/example/qsdmovies/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d("MainActivity", "onCreate")
+        Log.d("MainActivity", "Test Log, commit")
     }
 }
