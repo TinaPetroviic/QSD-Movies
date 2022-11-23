d1b80339b1d23e79469a6eba0fdd98c7402591fa
QSD_Tina <tina@qsdbih.com>
2022-11-23 16:26:02 +0100

    Create login and register

--- app/src/main/java/com/example/qsdmovies/ForgotActivity.kt
+++ app/src/main/java/com/example/qsdmovies/ForgotActivity.kt
@@ -3,6 +3,7 @@ package com.example.qsdmovies
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -23,7 +24,18 @@ class ForgotActivity : AppCompatActivity() {
         emailForgot = findViewById(R.id.emailForgot)
         sendEmailButton = findViewById(R.id.sendEmailButton)
 
-        auth = Firebase.auth
+        auth = FirebaseAuth.getInstance()
+
+        sendEmailButton.setOnClickListener {
+            val email = emailForgot.text.toString()
+            auth.sendPasswordResetEmail(email)
+                .addOnSuccessListener {
+                    Toast.makeText(this,"Please Check your Email",Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+                }
+        }
 
         val actionbar = supportActionBar
         actionbar!!.title = "BACK"
--- app/src/main/java/com/example/qsdmovies/RegisterActivity.kt
+++ app/src/main/java/com/example/qsdmovies/RegisterActivity.kt
@@ -1,14 +1,22 @@
 package com.example.qsdmovies
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var firstNameHere: EditText
@@ -17,7 +25,12 @@ class RegisterActivity : AppCompatActivity() {
     private lateinit var passwordRegister: EditText
     private lateinit var confirmPasswordRegister: EditText
     private lateinit var registerButton : Button
+    private lateinit var imageProfilePicture : ImageView
+    private lateinit var addProfilePicture : TextView
 
+    private  var storageRef = Firebase.storage
+
+    private lateinit var uri: Uri
     private lateinit var auth: FirebaseAuth
 
 
@@ -26,12 +39,13 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        storageRef = FirebaseStorage.getInstance()
+
         val actionbar = supportActionBar
         actionbar!!.title = "BACK"
 
         actionbar.setDisplayHomeAsUpEnabled(true)
         actionbar.setDisplayHomeAsUpEnabled(true)
-
 
         firstNameHere = findViewById(R.id.firstNameHere)
         lastNameHere = findViewById(R.id.lastNameHere)
@@ -39,6 +53,40 @@ class RegisterActivity : AppCompatActivity() {
         passwordRegister = findViewById(R.id.passwordRegister)
         confirmPasswordRegister = findViewById(R.id.confirmPasswordRegister)
         registerButton = findViewById(R.id.registerButton)
+        imageProfilePicture = findViewById(R.id.imageProfilePicture)
+        addProfilePicture = findViewById(R.id.addProfilePicture)
+
+        val galleryImage = registerForActivityResult(
+            ActivityResultContracts.GetContent(),
+            ActivityResultCallback {
+                imageProfilePicture.setImageURI(it)
+                uri = it
+            })
+
+        addProfilePicture.setOnClickListener {
+            galleryImage.launch("image/*")
+            storageRef.getReference("images").child(System.currentTimeMillis().toString())
+                .putFile(uri)
+                .addOnSuccessListener { task ->
+                    task.metadata!!.reference!!.downloadUrl
+                        .addOnSuccessListener {
+                            val userId = FirebaseAuth.getInstance().currentUser!!.uid
+                            val mapImage = mapOf(
+                                "url" to it.toString()
+                            )
+
+                            val databaseReference = FirebaseDatabase.getInstance().getReference("user Images")
+                            databaseReference.child(userId).setValue(mapImage)
+                                .addOnSuccessListener {
+                                    Toast.makeText(this,"Successful", Toast.LENGTH_SHORT).show()
+                                }
+                                .addOnFailureListener {
+                                    Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+                                }
+                        }
+                }
+        }
+
 
         auth = Firebase.auth
 
