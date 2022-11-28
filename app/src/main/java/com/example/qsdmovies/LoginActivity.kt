78e8adf528d6a0eda411b0f2b55acabf416602a1
QSD_Tina <tina@qsdbih.com>
2022-11-28 16:04:46 +0100

    Create login and register

--- app/src/main/java/com/example/qsdmovies/LoginActivity.kt
+++ app/src/main/java/com/example/qsdmovies/LoginActivity.kt
@@ -1,6 +1,6 @@
 package com.example.qsdmovies
 
-import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,8 +9,17 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
+
+@Suppress("DEPRECATION")
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var emailHere: EditText
@@ -21,10 +30,11 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var googleLogin: ImageView
     private lateinit var facebookLogin: ImageView
 
-
     private lateinit var auth: FirebaseAuth
 
-    @SuppressLint("MissingInflatedId")
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -39,6 +49,17 @@ class LoginActivity : AppCompatActivity() {
 
 
         auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+        googleLogin.setOnClickListener{
+            signIn()
+        }
 
         loginButton.setOnClickListener {
             login()
@@ -56,19 +77,57 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    private fun signIn() {
+        val signInIntent : Intent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result->
+            if (result.resultCode == Activity.RESULT_OK){
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResult(task)
+            }
+    }
+    private  fun handleResult(task: Task<GoogleSignInAccount>){
+        if (task.isSuccessful){
+            val account : GoogleSignInAccount? = task.result
+            if (account != null){
+                updateUI(account)
+            }
+
+        }else{
+            Toast.makeText(this,task.exception.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun updateUI(account: GoogleSignInAccount){
+        val credentials = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credentials).addOnCompleteListener{
+            if (it.isSuccessful){
+                val intent = Intent(this, HomeActivity::class.java)
+                intent.putExtra("email",account.email)
+                intent.putExtra("name",account.displayName)
+                startActivity(intent)
+
+            }else{
+                Toast.makeText(this,it.exception.toString(),Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
     private fun login() {
 
         val email = emailHere.text.toString()
         val pass = passwordHere.text.toString()
 
-        if(email.isEmpty() || pass.isEmpty()){
+        if (email.isEmpty() || pass.isEmpty()) {
             Toast.makeText(this, "Field can't be empty", Toast.LENGTH_SHORT).show()
             return
         }
 
         auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
             if (it.isSuccessful) {
-                val intent = Intent(this,HomeActivity::class.java)
+                val intent = Intent(this, HomeActivity::class.java)
                 startActivity(intent)
 
                 Toast.makeText(this, "Successfully LoggedIn", Toast.LENGTH_SHORT).show()
@@ -77,3 +136,5 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 }
+
+
